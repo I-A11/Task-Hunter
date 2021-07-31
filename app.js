eb7280223ecce356f859679866ff53294c9f4b09
I-A11 <d.islam1983@gmail.com>
2021-07-31 17:49:49 +1000

    update task-card section

--- app.js
+++ app.js
@@ -5,16 +5,3 @@ const toggleBtn = document.querySelector(".toggle");
 toggleBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
-
-// hero section
-const hero = document.querySelector(".hero");
-
-window.addEventListener("scroll", () => {
-  const scrollHeight = window.pageYOffset;
-  const heroHeight = hero.getBoundingClientRect().height;
-  if (scrollHeight > 100) {
-    hero.style.height = "100%";
-  } else {
-    hero.style.height = "#100vh";
-  }
-});
